da75bde6af98c6fe53ba9fe36c494a9ffda686a3
Mihai Cirlanaru <mcirlanaru@mozilla.com>
2012-10-22 15:36:47 -0700

    Added test for reading means file headers

--- js/test/emcc-test/hello_world.c
+++ js/test/emcc-test/hello_world.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#define BIO_HDRARG_MAX  32
 
 int main(int argc, char** argv) {
   if (argc < 2 ) {
@@ -17,5 +19,59 @@ int main(int argc, char** argv) {
     printf("File opened successfully!\n");
   }
 
+  // sphinxbase/bio.c test --------------------------------
+  int i, l, lineno;
+  char line[16384], word[4096];
+
+  lineno = 0;
+    if (fgets(line, sizeof(line), fp) == NULL){
+        printf("Premature EOF, line %d\n", lineno);
+        goto error_out;
+    }
+  lineno++;
+
+  if ((line[0] == 's') && (line[1] == '3') && (line[2] == '\n')) {
+      for (i = 0;;) {
+          if (fgets(line, sizeof(line), fp) == NULL) {
+              printf("Premature EOF, line %d\n", lineno);
+              goto error_out;
+          }
+          lineno++;
+
+          if (sscanf(line, "%s%n", word, &l) != 1) {
+              printf("Header format error, line %d\n", lineno);
+              goto error_out;
+          }
+          printf("[DEBUG] word 1: %s, l: %d\n", word, l);
+
+          if (strcmp(word, "endhdr") == 0)
+              break;
+          if (word[0] == '#') /* Skip comments */
+              continue;
+
+          if (i >= BIO_HDRARG_MAX) {
+              printf("Max arg-value limit(%d) exceeded; increase BIO_HDRARG_MAX\n",
+                   BIO_HDRARG_MAX);
+              goto error_out;
+          }
+
+          //(*argname)[i] = ckd_salloc(word);
+          if (sscanf(line + l, "%s", word) != 1) {      /* Multi-word values not allowed */
+              printf("Header format error, line %d\n", lineno);
+              goto error_out;
+          }
+
+          printf("[DEBUG] word 2: %s\n", word);
+          //(*argval)[i] = ckd_salloc(word);
+          i++;
+          printf("Iter: %d, lineno: %d\n", i, lineno);
+      }
+  }
+
+  printf(">> Success!\n");
+
   return 0;
+  error_out:
+    printf("ERROR!\n");
+    return 1;
 }
--- js/test/emcc-test/test.sh
+++ js/test/emcc-test/test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Usage: `basename $0` file"
+  exit 1
+fi
+
+FILE=$1
 # Test js compilation of simple C program that opens file with fopen
 # 1. Compile with emcc
 echo "-- Compiling with emcc ..."
-emcc -O2 --closure 0 --embed-file model/hmm/en/tidigits/mdef hello_world.c -o hello_world.js
+emcc -O2 --closure 0 --embed-file $FILE hello_world.c -o hello_world.js
 # 2. Run with node
 echo "-- Running compiled js ..."
-node hello_world.js model/hmm/en/tidigits/mdef
+node hello_world.js $FILE
