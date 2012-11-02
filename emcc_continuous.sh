8272cb756eedc32642b75e0d13a7cdc975856174
Mihai Cirlanaru <mcirlanaru@mozilla.com>
2012-11-02 15:54:31 -0700

    Changed compile parameters to global variables in emcc_continuous script

--- emcc_continuous.sh
+++ emcc_continuous.sh
@@ -13,6 +13,14 @@ JS_PROGR=pocketsphinx_continuous.js
 
 RAW_FILE=test/data/numbers.raw
 
+# Acoustic and language models parameters
+HMM_DIR=model/hmm/en/tidigits # Tidigits acoustic model
+LM_DIR=model/lm/en
+DICT=tidigits.dic
+DMP=tidigits.DMP
+FSG=tidigits.fsg
+
+
 POCKETSPHINX_ROOT=.
 ARGS="-O2 --closure 0"
 ARGS_SAFE="-O0 --closure 0 -s SAFE_HEAP=1" 
@@ -26,20 +34,20 @@ emcc $ARGS \
 	$SPHINXBASE_DIR/src/libsphinxad/*.o \
 	src/programs/continuous.o \
 	--embed-file $RAW_FILE \
-	--embed-file model/hmm/en/tidigits/mdef \
-	--embed-file model/hmm/en/tidigits/transition_matrices \
-	--embed-file model/hmm/en/tidigits/variances \
-	--embed-file model/hmm/en/tidigits/sendump \
-	--embed-file model/hmm/en/tidigits/feat.params \
-	--embed-file model/hmm/en/tidigits/means \
-	--embed-file model/lm/en/tidigits.DMP \
-	--embed-file model/lm/en/tidigits.dic \
-	--embed-file model/lm/en/tidigits.fsg \
+	--embed-file $HMM_DIR/mdef \
+	--embed-file $HMM_DIR/transition_matrices \
+	--embed-file $HMM_DIR/variances \
+	--embed-file $HMM_DIR/sendump \
+	--embed-file $HMM_DIR/feat.params \
+	--embed-file $HMM_DIR/means \
+	--embed-file $LM_DIR/$DMP \
+	--embed-file $LM_DIR/$DICT \
+	--embed-file $LM_DIR/$FSG \
 	-o $JS_DIR/$JS_PROGR
 
 echo "-- Running $JS_PROGR on $RAW_FILE ..."
 node $JS_DIR/$JS_PROGR -infile $RAW_FILE \
-	-hmm model/hmm/en/tidigits \
-	-dict model/lm/en/tidigits.dic \
-	-lm model/lm/en/tidigits.DMP
+	-hmm $HMM_DIR \
+	-dict $LM_DIR/$DICT \
+	-lm $LM_DIR/$DMP
 
